93480679461d7ec1ab0487aa009d898517ed6f86
entfane <nikita.kezins@gmail.com>
2024-06-10 23:48:59 +0200

    Initial files

---
+++ src/main/java/com/example/Personal_Finance_Management_System/PersonalFinanceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Personal_Finance_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceManagementSystemApplication.class, args);
+	}
+
+}
