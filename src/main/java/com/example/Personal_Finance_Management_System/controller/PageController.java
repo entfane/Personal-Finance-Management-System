0d8023d50c66ed6264621b80930e2634ffd268ff
Nikita Kezins <n.kezins@student.utwente.nl>
2024-06-18 15:02:05 +0200

    Fixed PageController

---
+++ src/main/java/com/example/Personal_Finance_Management_System/controller/PageController.java
@@ -0,0 +1,32 @@
+package com.example.Personal_Finance_Management_System.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/")
+public class PageController {
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String getRegistrationPage() {
+        return "registration";
+    }
+
+    @GetMapping("/dashboard")
+    public String getDashboard() {
+        return "dashboard";
+    }
+
+    @GetMapping("/transaction")
+    public String getTransactions() {
+        return "transaction";
+    }
+
+}
