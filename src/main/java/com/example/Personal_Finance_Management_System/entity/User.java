dbe441cb4a4a4528125495ffcfe904b4808ace80
Nikita Kezins <n.kezins@student.utwente.nl>
2024-07-01 18:32:03 +0200

    Created User entity

---
+++ src/main/java/com/example/Personal_Finance_Management_System/entity/User.java
@@ -0,0 +1,50 @@
+package com.example.Personal_Finance_Management_System.entity;
+
+import jakarta.persistence.*;
+
+@Entity(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    public User() {}
+
+    public User(int id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
